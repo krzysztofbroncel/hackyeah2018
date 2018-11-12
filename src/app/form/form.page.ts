68e6a12513c27fa4dfbeefbf7fcdcd2a5c994afa
bronek <krzysztof.broncel@gmail.com>
2018-11-12 20:47:00 +0100

    Simple LogIn form added to Form page

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['form.page.scss'],
 })
 export class FormPage {
+  
+  username: string;
+  password: string;
 
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  login() {
+    console.log(`Logging with user ${this.username} and password ${this.password}`);
+  }
+
+  goHome() {
+    this.navCtrl.navigateForward('home');
+  }
 
 }
